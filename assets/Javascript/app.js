ce928a52e851e098d6965bf530cd29b56c548592
Aaron Jackson <aarontjackson@outlook.com>
2019-09-21 17:15:26 -0400

    Google map with restuarant markers surrounding current location

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -156,11 +156,125 @@ $(document).ready(function () {
                     $("#contentContainer").append(newDiv);
                 }
 
-
+                initMap();
 
             });
-    })
+
+        var map;
+        var infoWindow;
+        // var service;
+
+        function initMap() {
+            // origin = 28.3852, 81.56
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 28.3852, lng: 81.5639 },
+                zoom: 16
+            });
+
+            infoWindow = new google.maps.InfoWindow;
+
+            // Try HTML5 geolocation.
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    console.log(pos.lat);
+                    console.log(pos.lng);
+
+                    findPlaces(pos.lat, pos.lng);
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent('Location found.');
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                }, function () {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                });
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, infoWindow, map.getCenter());
+            }
+            // var request = {
+            //     location: map.center,
+            //     radius: "500",
+            //     type: ["restaurant"]
+            // };
+
+            // service = new google.maps.places.PlacesService(map);
+            // service.nearbySearch(request, callback);
+
+            // function callback(results, status) {
+            //     if (status == google.maps.places.PlacesServiceStatus.OK) {
+            //         for (var i = 0; i < results.length; i++) {
+            //             var place = results[i];
+            //             createMarker(results[i]);
+            //         }
+
+            //     }
+            // };
+
+            function findPlaces(lat, lng) {
+
+                $.ajax({
+                    url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng + "&radius=1500&type=restaurant&key=AIzaSyBalsCc3sNclUyPdty0itcxzXTBs9pypoo",
+                    method: "GET"
+                }).then(function (response) {
+                    console.log(response.results);
+
+                    var locations = [];
+                    for (let index = 0; index < response.results.length; index++) {
+                        var currentInfo = [response.results[index].name, response.results[index].geometry.location.lat, response.results[index].geometry.location.lng]
+                        locations.push(currentInfo);
+                    }
+                    console.log(locations);
+
+
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                        zoom: 15,
+                        center: new google.maps.LatLng(lat, lng),
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    });
+
+                    var infowindow = new google.maps.InfoWindow();
+
+                    var marker, i;
+
+                    for (i = 0; i < locations.length; i++) {
+                        marker = new google.maps.Marker({
+                            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                            map: map
+                        });
+
+                        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                            return function () {
+                                infowindow.setContent(locations[i][0]);
+                                infowindow.open(map, marker);
+                            }
+                        })(marker, i));
+                    }
+
+
+                });
+
+            }
+        };
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(browserHasGeolocation ?
+                'Error: The Geolocation service failed.' :
+                'Error: Your browser doesn\'t support geolocation.');
+            infoWindow.open(map);
+        }
+
+    });
+
+
+
 });
+
 
 
 
